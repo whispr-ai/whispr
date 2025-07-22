4913aaa958c781bb7efef0d56cc5e50f2ec8117a
Timothy Lau <git@timlau.me>
2025-07-22 23:36:43 +0800

    Initial Commit

---
+++ whispr/whisprApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisprApp.swift
+//  whispr
+//
+//  Created by 刘沛强 on 2025/7/22.
+//
+
+import SwiftUI
+
+@main
+struct whisprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ whisprTests/whisprTests.swift
@@ -0,0 +1,17 @@
+//
+//  whisprTests.swift
+//  whisprTests
+//
+//  Created by 刘沛强 on 2025/7/22.
+//
+
+import Testing
+@testable import whispr
+
+struct whisprTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
